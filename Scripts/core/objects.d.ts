56972dd78ba3be9e0554420251939264e13efaec
awen-xu <wxu59@my.centennialcollege.ca>
2018-07-11 16:23:34 -0400

    end of lab class

--- Scripts/core/objects.d.ts
+++ Scripts/core/objects.d.ts
@@ -1,3 +1,48 @@
 declare module objects {
+     /**
+         * Creates an instance of Button
+         * @param {*} sourcePath 
+         * @param {*} width 
+         * @param {*} height 
+         */
+    export class Button{
+        constructor(source:string = "", width:number = 0, height:number = 0)
+    }
+
+    export class Vector{
+        /**
+         *Creates an instance of Vector.
+         * @param {number} [x=0]
+         * @param {number} [y=0]
+         */
+        constructor(x:number=0, y:number=0);
+
+        /**
+         * This method returns string representation of a vector
+         * @returns {string}
+         */
+        public toString():string;
+
+        /**
+         * This method returns the distance of two vectors
+         *
+         * @static
+         * @param {Vector} vec1
+         * @param {Vector} vec2
+         * @returns {number}
+         */
+        public static distance(vec1:Vector, vec2:Vector):number;
+
+        /**
+         * This method returns a new Vector that is the sum of two vectors
+         *
+         * @static
+         * @param {Vector} vec1
+         * @param {Vector} vec2
+         * @returns {Vector}
+         * @memberof Vector
+         */
+        public static add(vec1:Vector, vec2:Vector):Vector;
+    }
 
 }
--- Scripts/objects/button.js
+++ Scripts/objects/button.js
@@ -1,6 +1,27 @@
 let objects;
 (function(objects){
 
+    class Button extends Image{
+
+       
+        constructor(sourcePath = "", width = 0, height = 0){
+            super(width, height);
+
+            this.src = sourcePath;
+
+            this.addEventListener("mouseover", function(){
+                this.style.opacity = 0.7; // sets the alpha transparency to 30%
+
+            });
+
+            this.addEventListener("mouseout", function(){
+                this.style.opacity = 1.0; // sets the alpha transparency to 0%
+
+            });
+        }
+
+    }
 
 
+    objects.Button = Button;
 })(objects || (objects = {}));
